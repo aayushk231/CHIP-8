26d48846eca38843d2797bdcb717f9e2a401c9ed
Aayush Kushwaha <aayushkushwaha4@gmail.com>
2025-03-05 19:07:12 +0530

    Added a screen for output

--- chip8.py
+++ chip8.py
@@ -1,1 +1,15 @@
-#testting out git
+import pygame
+from pygame.locals import *
+
+width = 64
+height = 32
+
+pygame.init()
+screen = pygame.display.set_mode((width*10, height*10))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        
+        if event.type == QUIT:
+            run = False
